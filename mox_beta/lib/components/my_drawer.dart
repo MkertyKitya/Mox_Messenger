3b3a24fcf3ed90a5a00b7e52417d8bc1054a6f37
Mkerty <kitinmisha@gmail.com>
2026-02-12 15:37:02 +0300

    Feat: My Drawer update

--- mox_beta/lib/components/my_drawer.dart
+++ mox_beta/lib/components/my_drawer.dart
@@ -1,1 +1,24 @@
 import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      child: Column(
+        children: [
+          // logo
+          DrawerHeader(
+            child: Center(child: Icon(Icons.message)),
+            // DrawerHeader
+            // home list tile
+            //settings list tile
+            // logout list tile
+          ),
+        ],
+      ),
+    );
+  }
+}
