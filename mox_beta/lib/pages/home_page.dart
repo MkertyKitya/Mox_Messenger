1a0a97465530ff8edb65c7a63c3a49b30623fb05
Mkerty <kitinmisha@gmail.com>
2026-02-13 10:53:19 +0300

    Feat: DEL Home Page void LogOut

--- mox_beta/lib/pages/home_page.dart
+++ mox_beta/lib/pages/home_page.dart
@@ -1,26 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:mox_beta/auth/auth_service.dart';
 import 'package:mox_beta/components/my_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  void logout() {
-    //get auth service
-    final auth = AuthService();
-    auth.signOut();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-        actions: [
-          // logout button
-          IconButton(onPressed: logout, icon: Icon(Icons.logout)),
-        ],
-      ),
+      appBar: AppBar(title: Text('Home')),
       drawer: MyDrawer(),
     );
   }
