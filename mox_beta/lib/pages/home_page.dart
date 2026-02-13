11d6d3dbe476a0b7023840b6e01a0323d42b2756
Mkerty <kitinmisha@gmail.com>
2026-02-13 14:56:13 +0300

    Feat: user_tile

--- mox_beta/lib/pages/home_page.dart
+++ mox_beta/lib/pages/home_page.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:mox_beta/components/my_drawer.dart';
+import 'package:mox_beta/services/auth/auth_service.dart';
+import 'package:mox_beta/services/chat/chat_service.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final ChatService _chatService = ChatService();
+  final AuthService _authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home')),
-      drawer: MyDrawer(),
+      appBar: AppBar(title: const Text('Home')),
+      drawer: const MyDrawer(),
+      body: _buildUserList(),
     );
+  }
+
+  // build a list of users except for the current logged in user
+  Widget _buildUserList() {
+    return StreamBuilder(
+      stream: _chatService.getUsersStream(),
+      builder: (context, snapshot) {
+        // error
+        if (snapshot.hasError) {
+          return const Text("Error");
+        }
+
+        // loading..
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading..");
+        }
+
+        // return list view
+        return ListView(
+          children: snapshot.data!
+              .map<Widget>((userData) => buildUserListItem)
+              .toList(),
+        );
+      },
+    );
+  }
+
+  // build individual list tile for user
+  Widget _buildUserListItem(
+    Map<String, dynamic> userData,
+    BuildContext context,
+  ) {
+    // display all users except current user
+    return UserTile();
   }
 }
--- mox_beta/lib/services/chat/chat_service.dart
+++ mox_beta/lib/services/chat/chat_service.dart
@@ -1,3 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ChatService {
   // get instanse of firestore
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  // get user stream
+  /*
+  List<Map<String,dynamic> =
+  [
+  {
+  'email': test@gmail.com ,
+  'id': …
+  }.
+  {
+  'email': mitch@gmail.com ,
+  'id':
+  },
+  ]
+  */
+  Stream<List<Map<String, dynamic>>> getUsersStream() {
+    return _firestore.collection("Users").snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) {
+        // go through each individual user
+        final user = doc.data();
+
+        // return user
+        return user;
+      }).toList();
+    });
+  }
+  // get user stream
+
+  // send message
+
+  // get message
 }
