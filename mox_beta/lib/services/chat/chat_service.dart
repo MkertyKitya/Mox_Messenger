792eec25f9ac7a734ab63a29cda1925f902e0e4b
Mkerty <kitinmisha@gmail.com>
2026-02-13 19:23:22 +0300

    Feat: Message

--- mox_beta/lib/services/chat/chat_service.dart
+++ mox_beta/lib/services/chat/chat_service.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatService {
-  // get instanse of firestore
+  // get instanse of firestore & auth
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   // get user stream
   /*
   List<Map<String,dynamic> =
@@ -31,6 +33,12 @@ class ChatService {
   // get user stream
 
   // send message
+  Future<void> sendMessage(String receiverID, message) async {
+    // get current user info
+    final String currentUserID = _auth.currentUser!.uid;
+    final String currentUserEmail = _auth.currentUser!.email!;
+    final Timestamp timestamp = Timestamp.now();
+  }
 
   // get message
 }
