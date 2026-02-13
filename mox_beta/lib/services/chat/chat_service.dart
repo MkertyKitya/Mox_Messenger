fd47d4539c838d9a9b42ff9c2298bb864a1dd273
Mkerty <kitinmisha@gmail.com>
2026-02-14 00:36:22 +0300

    Feat: Chat Service: Chat room

--- mox_beta/lib/services/chat/chat_service.dart
+++ mox_beta/lib/services/chat/chat_service.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mox_beta/models/message.dart';
 
 class ChatService {
   // get instanse of firestore & auth
@@ -38,7 +39,41 @@ class ChatService {
     final String currentUserID = _auth.currentUser!.uid;
     final String currentUserEmail = _auth.currentUser!.email!;
     final Timestamp timestamp = Timestamp.now();
+
+    // create a new message
+    Message newMessage = Message(
+      senderID: currentUserEmail,
+      senderEmail: currentUserID,
+      receiverID: receiverID,
+      message: message,
+      timestamp: timestamp,
+    );
+
+    // construct chat room ID for the two users (sorted to ensure uniqueness)
+    List<String> ids = [currentUserID, receiverID];
+    ids.sort(); // sort the ids (this ensure the chatroomID is the same for any 2 people)
+    String chatRoomID = ids.join('_');
+
+    // add new message to database
+    await _firestore
+        .collection("chat_rooms")
+        .doc(chatRoomID)
+        .collection("messages")
+        .add(newMessage.toMap());
   }
 
-  // get message
+  // get messages
+  Stream<QuerySnapshot> getMessages(String userID, otherUserID) {
+    // construct a chatroom ID for the two users
+    List<String> ids = [userID, otherUserID];
+    ids.sort();
+    String chatRoomID = ids.join('_');
+
+    return _firestore
+        .collection("chat_rooms")
+        .doc(chatRoomID)
+        .collection("messages")
+        .orderBy("timestamp", descending: false)
+        .snapshots();
+  }
 }
