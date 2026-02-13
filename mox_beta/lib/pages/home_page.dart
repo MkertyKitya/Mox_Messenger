ca07acabbb6ac54e8076cfa7d1617908348527b6
Mkerty <kitinmisha@gmail.com>
2026-02-13 19:15:10 +0300

    Feat: User Tile

--- mox_beta/lib/pages/home_page.dart
+++ mox_beta/lib/pages/home_page.dart
@@ -51,17 +51,22 @@ class HomePage extends StatelessWidget {
     BuildContext context,
   ) {
     // display all users except current user
-    return UserTile(
-      text: userData["email"],
-      onTap: () {
-        // tapped on a user -> go to chat page
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => ChatPage(receiverEmail: userData["email"]),
-          ),
-        );
-      },
-    );
+    if (userData["email"] != _authService.getCurrentUser()!.email) {
+      return UserTile(
+        text: userData["email"],
+        onTap: () {
+          // tapped on a user -> go to chat page
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) =>
+                  ChatPage(receiverEmail: userData["email"]), // ChatPage
+            ), // MaterialPageRoute
+          );
+        },
+      ); // UserTile
+    } else {
+      return Container();
+    }
   }
 }
