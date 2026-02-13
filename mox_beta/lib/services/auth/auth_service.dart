8117de993cc0f1423099f718981d96639176646c
Mkerty <kitinmisha@gmail.com>
2026-02-13 18:54:05 +0300

    Feat: Home Page

--- mox_beta/lib/services/auth/auth_service.dart
+++ mox_beta/lib/services/auth/auth_service.dart
@@ -1,8 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   // instance of auth
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   // sign in
   Future<UserCredential> signInWithEmailPassword(String email, password) async {
@@ -11,6 +13,13 @@ class AuthService {
         email: email,
         password: password,
       );
+
+      // save user info if it doesn't already exist
+      _firestore.collection("Users").doc(userCredential.user!.uid).set({
+        'uid': userCredential.user!.uid,
+        'email': email,
+      });
+
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
@@ -22,6 +31,12 @@ class AuthService {
     try {
       UserCredential userCredential = await _auth
           .createUserWithEmailAndPassword(email: email, password: password);
+
+      // save user info in a separate doc
+      _firestore.collection("Users").doc(userCredential.user!.uid).set({
+        'uid': userCredential.user!.uid,
+        'email': email,
+      });
 
       return userCredential;
     } on FirebaseAuthException catch (e) {
